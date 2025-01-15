540b577782b0ff988a9fb790018bb0d71c818634
Nickgiresse <kamdemnick12@gmail.com>
2025-01-15 22:13:25 +0000

    mise a jour

--- Index.php
+++ Index.php
@@ -42,12 +42,100 @@
 </nav>
 
 <main>
+
     <div class="image-section w-full h-full">
         <div class="text-content w-full h-full">
             <h1 class="text-3xl font-bold mb-4">Bienvenue sur l'espace de parrainage IAI-CAMEROUN Centre de DOUALA</h1>
         </div>
     </div>
+    <div>
+        <div>
+            <img src="uploads/" alt="">
+        </div>
+    </div>
+    <section class="bg-[#FF7F50d7] h-max " >
+        <h1 class="text-white text-center text-4xl font-bold pt-10 ">Quel sont nos activite?</h1>
+        <div class="grid grid-cols-3 grid-rows-2 gap-6 p-4">
+            <div class=" p-2 rounded ">
+                <img src="uploads/culturel.jpg" alt="" class="border-8 border-white rounded-md">
+        
+            </div>
+            <div class=" p-2 row-span-3 rounded ">
+                <img src="uploads/soiree.webp" alt="" class="border-8 border-white rounded-md">
+               
+            </div>
+            <div class=" p-2 rounded ">
+                <img src="uploads/chanson.jpg" alt="" class="border-8 border-white rounded-md">
+               
+            </div>
+            <div class=" p-2 rounded ">
+                <img src="uploads/buffet.jpeg" alt="" class="border-8 border-white rounded-md">
+               
+            </div>
+            <div class=" p-2 rounded ">
+                <img src="uploads/miss.png" alt="" class=" rounded-md">
+                
+            </div>
+            
 
+        </div>
+        <p class="text-content">Soirée de parrinage prevus initialement le 1er fevrier 2025. 
+Au programme tous plein d’activiter
+Miss/Master, Danser et festivite,Boisson et collation</p>
+        <p class="flex justify-center w-full"><a href="#" class=" bg-white text-[#FF7F50d7] flex w-1/6 text-center flex flex-row justify-center p-3 rounded-md m-3 font-bold">En savoir plus</a></p>
+    </section>
+    <section>
+        <h1 class="text-center text-4xl font-bold pt-10">Qui sommes nous?</h1>
+        <div class="flex flex-rows justify-between items-center gap-8 p-4">
+            <img class="w-72 " src="uploads/rr.png" alt="">
+            <div class=" flex flex-col justify-center items-center p-4">
+                <p class="w-3/4">Fonder par mr <span class="font-bold text-[#FF7F50d7]">Armand Claude abanda</span>,
+IAI signifie Institue Africaine D’Informatique
+forme de nombreux ingenieur chaque annee <br> L’I.A.I-Cameroun, Représentation Camerounaise de l’I.A.I, existe depuis le 1er octobre 1999 : création réalisée par l’Etat Camerounais sous la haute impulsion du Président de la République, Son Excellence Paul BIYA qui a instruit aussitôt la signature d’un accord de siège entre l’Institut Africain d’Informatique et la République camerounaise, dotant l’IAI-Cameroun d’un statut de mission diplomatique.
+</p>            
+                <a href="#" class="border-2 border-[#FF7F50d7] text-[#FF7F50d7] font-bold p-2 rounded-3xl hover:bg-[#FF7F50d7] hover:text-white">Voir plus</a>
+            </div>
+        </div>
+    </section>
+    <section class="bg-[#FF6347] h-max text-white m-10 p-5">
+        <div class="flex flex-col justify-center items-center gap-4 p-4 md:flex-row lg:flex-row">
+            <img class="w-2/5" src="uploads/reseau.jpeg" alt="">
+            <div >
+                <h1 class="font-bold ">Systeme et Reseau</h1>
+                <p>Les systèmes et les réseaux forment 
+ensemble le cœur de l'infrastructure 
+informatique moderne. 
+Les systèmes informatiques traitent
+ les données, exécutent des applications
+, et fournissent des services, 
+tandis que les réseaux permettent de 
+connecter ces systèmes entre eux 
+pour échanger des informations</p>
+            </div>
+            
+        </div><br>
+
+        <div class="flex flex-col justify-center items-center gap-4 p-4 md:flex-row lg:flex-row">
+           
+            <div >
+                <h1 class="font-bold hidden md:block lg:block">Genie Logiciel</h1>
+                <p class="hidden md:block lg:block">Le génie logiciel est la discipline de 
+l'informatique qui se concentre sur 
+la conception, le développement, le 
+test, la maintenance et la gestion des 
+logiciels.</p>
+            </div>
+            <img class="w-2/5" src="uploads/genie-logiciel.jpg" alt="">
+            <h1 class=" b-black text-left font-bold block md:hidden lg:hidden">Genie Logiciel</h1>
+                <p class=" block md:hidden lg:hidden">Le génie logiciel est la discipline de 
+l'informatique qui se concentre sur 
+la conception, le développement, le 
+test, la maintenance et la gestion des 
+logiciels.</p>
+            </div>
+        </div>
+
+    </section>
     <section class="my-8 h-64 flex flex-col justify-center items-center">
         <p class="text-center text-gray-900 mb-4">Commencer cette aventure avec nous</p>
         <div class="flex justify-center items-center gap-8">
--- script/index.js
+++ script/index.js
@@ -1,10 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.querySelector('.burger_menu');
     const sidebar = document.querySelector('.sidebar');
-
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+    function balise() {
+        table = [main, footer];
+        table.forEach(element=> {
+            element.addEventListener('click', () => {
+                sidebar.classList.remove('open');
+                burgerMenu.classList.remove('active');
+                console.log("bien bien");
+            });
+        });
+        
+    }
     burgerMenu.addEventListener('click', () => {
         sidebar.classList.toggle('open');
         burgerMenu.classList.toggle('active');
-        console.log("erreur")
+        
+        console.log("erreur&")
     });
+    // main.addEventListener('click', () => {
+    //     sidebar.classList.remove('open');
+    //     burgerMenu.classList.remove('active');
+    //     console.log("bien bien");
+        
+    // });
+    balise(main, footer);
+    
 });
